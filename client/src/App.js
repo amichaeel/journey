2b2f94bfd94c120359d5c9704facf16981e35ca4
Kristian <kristiangcorrea@live.com>
2023-09-16 10:33:05 -0400

    Navbar now exists throughout entire website. Tweaked some css to allow for navbar exist. Replaced class property in tags with the proper className property.

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,25 @@ import Home from "./pages/Home/Home";
 import Dashboard from "./pages/Dashboard/Dashboard";
 
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import TopNavBar from "./components/TopNavBar/TopNavBar";
+import "./App.css"
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </Router>
+        <div className="app-container">
+            <TopNavBar />
+            <div className="app-body">
+                <div >
+                    <Router>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/dashboard" element={<Dashboard />} />
+                        </Routes>
+                    </Router>
+                </div>
+            </div>
+        </div>
     );
   }
 }
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,7 +1,10 @@
 
 function Dashboard() {
     return (
-        <h1>Dashboard</h1>
+        <div>
+            <h1>Previous Journeys</h1>
+            <h1>Create a Journey</h1>
+        </div>
     );
 }
 
