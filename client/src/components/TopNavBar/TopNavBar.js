3a748ae45eda4053b2e8e4fb2f7f379c7b7e3cdf
Anthony Ham <mail@anthonymham.com>
2023-09-16 09:54:32 -0400

    auth0 working + spinner loader

--- client/src/components/TopNavBar/TopNavBar.js
+++ client/src/components/TopNavBar/TopNavBar.js
@@ -1,17 +1,27 @@
 import "./TopNavBar.css";
 import LoginButton from "../NavButton/LoginButton";
 import SignupButton from "../NavButton/SignupButton";
+import { useAuth0 } from "@auth0/auth0-react";
+import LogoutButton from "../NavButton/LogoutButton";
+import Spinner from "../Spinner/Spinner"
 
 function TopNavBar() {
-  return (
-    <nav class="navbar navbar-top">
-      <h1 class="navtitle">Journey</h1>
-      <ul class="navmenu">
-        <LoginButton />
-        <SignupButton />
-      </ul>
-    </nav>
-  );
+    const { isLoading, error } = useAuth0();
+    return (
+        <nav class="navbar navbar-top">
+            <h1 class="navtitle">Journey</h1>
+            <ul class="navmenu">
+                {error && <p>Authentication Error</p>}
+                {!error && isLoading && <Spinner />}
+                {!error && !isLoading && (
+                    <>
+                        <LoginButton />
+                        <LogoutButton />
+                    </>
+                )}
+            </ul>
+        </nav>
+    );
 }
 
 export default TopNavBar;
